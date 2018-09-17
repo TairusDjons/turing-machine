3e140e2b0dff553ce67f0da0836cf73f9c32c9a6
Vladislav Soloduhin <solodyxin.1997@gmail.com>
2018-09-17 23:54:28 +0300

    add open file dialog service

--- Turing.WPF/App.xaml.cs
+++ Turing.WPF/App.xaml.cs
@@ -10,6 +10,8 @@ using Autofac.Extras.CommonServiceLocator;
 using CommonServiceLocator;
 using Turing.IO;
 using Turing.ViewModels;
+using Turing.IService;
+using Turing.WPF.Services;
 
 namespace Turing.WPF
 {
@@ -23,6 +25,7 @@ namespace Turing.WPF
             var builder = new ContainerBuilder();
             builder.RegisterType<TuringMachineFactory>().As<ITuringMachineFactory>().SingleInstance();
             builder.RegisterType<TuringCommandParser>().As<ITuringCommandParser>().SingleInstance();
+            builder.RegisterType<OpenFileDialogService>().As<IOpenFileDialogService>().SingleInstance();
             builder.RegisterType<MainViewModel>().SingleInstance();
             var container = builder.Build();
             var csl = new AutofacServiceLocator(container);
--- Turing/IService/IOpenFileDialogService.cs
+++ Turing/IService/IOpenFileDialogService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Turing.Services
+namespace Turing.IService
 {
     public interface IOpenFileDialogService
     {
