b0bda252d3c2e80f3f00bab31ad0b450b832d296
Vladislav Soloduhin <solodyxin.1997@gmail.com>
2018-09-17 23:18:04 +0300

    add open file dialog service interface

---
+++ Turing/IService/IOpenFileDialogService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turing.Services
+{
+    public interface IOpenFileDialogService
+    {
+        void Open();
+    }
+}
